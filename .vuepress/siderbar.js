0c9ca939ba937d372bdde501be0ebb450153e0e8
路明泽 <2250206701@qq>
2024-03-06 20:42:57 +0800

    watch实现,/新分类

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -3,7 +3,7 @@ const JAVASCRIPT_PATH = '/blogs/javascript'
 const CSS_PATH = '/blogs/css'
 const PERFORM_PATH = '/blogs/perform'
 const ALGORITHM_PATH = '/blogs/vuecode'
-const GIT_PATH = '/blogs/git'
+const KNOWLEDGE_PATH = '/blogs/knowledge'
 const HTML_PATH = '/blogs/html'
 const JQ_PATH = '/blogs/jq'
 const OTHER_PATH = '/blogs/other'
@@ -14,7 +14,7 @@ module.exports = {
   [CSS_PATH]: [createSideBarConfig('CSS基础', CSS_PATH)],
   [PERFORM_PATH]: [createSideBarConfig('前端性能相关', PERFORM_PATH)],
   [ALGORITHM_PATH]: [createSideBarConfig('vue核心源码', ALGORITHM_PATH)],
-  [GIT_PATH]: [createSideBarConfig('git', GIT_PATH)],
+  [KNOWLEDGE_PATH]: [createSideBarConfig('每天一个知识点', KNOWLEDGE_PATH)],
   [HTML_PATH]: [createSideBarConfig('HTML', HTML_PATH)],
   [JQ_PATH]: [createSideBarConfig('jq', JQ_PATH)],
   [OTHER_PATH]: [createSideBarConfig('工具', OTHER_PATH)],
