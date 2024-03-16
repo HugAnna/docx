e58300160224a742f0051431d1f887a5bb98b99f
路明泽 <2250206701@qq>
2024-03-16 11:20:58 +0800

    新增webpack

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -7,15 +7,16 @@ const KNOWLEDGE_PATH = '/blogs/knowledge'
 const HTML_PATH = '/blogs/html'
 const JQ_PATH = '/blogs/jq'
 const OTHER_PATH = '/blogs/other'
-
+const INTERVIEW_PATH = '/blogs/interview'
 
 module.exports = {
   [JAVASCRIPT_PATH]: [createSideBarConfig('js基础', JAVASCRIPT_PATH)],
   [CSS_PATH]: [createSideBarConfig('CSS基础', CSS_PATH)],
   [PERFORM_PATH]: [createSideBarConfig('前端性能相关', PERFORM_PATH)],
   [ALGORITHM_PATH]: [createSideBarConfig('vue核心源码', ALGORITHM_PATH)],
-  [KNOWLEDGE_PATH]: [createSideBarConfig('每天一个知识点', KNOWLEDGE_PATH)],
+  [KNOWLEDGE_PATH]: [createSideBarConfig('杂项', KNOWLEDGE_PATH)],
   [HTML_PATH]: [createSideBarConfig('HTML', HTML_PATH)],
   [JQ_PATH]: [createSideBarConfig('jq', JQ_PATH)],
   [OTHER_PATH]: [createSideBarConfig('工具', OTHER_PATH)],
+  [INTERVIEW_PATH]: [createSideBarConfig('面试题', INTERVIEW_PATH)],
 }
